145606e7f7208fe8e634e5cc6b35ce069859552b
MateusRNM <mateusmt676@gmail.com>
2025-10-14 12:32:32 -0400

    swagger

--- src/modules/option/dto/create-option.dto.ts
+++ src/modules/option/dto/create-option.dto.ts
@@ -1,13 +1,27 @@
-import { IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreateOptionDto {
+    
     @IsString()
+    @IsNotEmpty()
+    @ApiProperty({
+        description: 'Nome da opção'
+    })
     name: string;
 
     @IsString()
+    @IsNotEmpty()
+    @ApiProperty({
+        description: 'Descrição da opção'
+    })
     description: string;
 
     @IsString()
+    @IsNotEmpty()
+    @ApiProperty({
+        description: 'Id do scenário correspondente'
+    })
     scenarioId: string;
     
     //scores: Score[]
--- src/modules/scenario/dto/create-scenario.dto.ts
+++ src/modules/scenario/dto/create-scenario.dto.ts
@@ -1,11 +1,21 @@
-import { IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreateScenarioDto {
+
     @IsString()
+    @IsNotEmpty()
+    @ApiProperty({
+        description: 'Título do scenario'
+    })
     title: string;
 
     @IsString()
+    @IsNotEmpty()
+    @ApiProperty({
+        description: 'Descrição do scenario'
+    })
     description: string;
-    
+
     //options: Option[];
 }
